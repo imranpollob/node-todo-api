b0746a0eb5e014722f2435a28820a838292680e5
pollmix <polboy777@gmail.com>
2018-11-25 12:58:27 +0600

    added post request

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
 const { Todo } = require("./model/Todo");
 const { User } = require("./model/User");
 
-const newTodo = new Todo({
-  text: "First Mongoose Task"
-});
+const app = express()
+app.use(bodyParser.json())
 
-const newUser = new User({
-  name: "Pollob"
-});
+app.get('/', (req, res) => {
+  res.send('Ok')
+})
 
-newTodo.save().then(res => console.log(res), err => console.log(err));
-newUser.save().then(res => console.log(res), err => console.log(err));
+app.post('/todos', (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then(doc => res.send(doc), err => res.status(422).send(err));
+})
+
+app.post('/users', (req, res) => {
+  const newUser = new User({
+    name: req.body.name
+  });
+
+  newUser.save().then(doc => res.send(doc), err => res.status(422).send(err));
+})
+
+app.listen(5000, () => console.log('Server starting on port 5000...'))
