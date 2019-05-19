d1b4fcfe103c50b5083fe06b740cfa86d324ecba
pollmix <polboy777@gmail.com>
2019-05-19 10:27:27 +0600

    added to mlab

--- mongoose.js
+++ mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
+mongoose.connect("mongodb://todo:todo123@ds159036.mlab.com:59036/todo-api123", { useNewUrlParser: true });
 
 mongoose.connection
   .on('error', console.error.bind(console, 'Connection Error:'))
